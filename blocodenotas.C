2bb41b08d6678f8b43311478f05703627ba2c682
JMiguelMestre <JMiguelMestre@users.noreply.github.com>
2016-12-28 16:37:43 +0000

    Update blocodenotas.C

--- blocodenotas.C
+++ blocodenotas.C
@@ -48,12 +48,13 @@ void mostrarpartid(tParlamento x,int np, int nd){
     }
 }
 
-void mostrardep (tParlamento x, int np,int nd){
+void mostrardep (tParlamento x, int np){
     int i,j;
     for(i=0;i<np;i++){
-        printf("%s\n",x.partidos[i].sigla);
-        for(j=0;j<nd;j++){
-            printf("%d - %s  - %c - %s\n", x.partidos[i].deputados[j].codigo, x.partidos[i].deputados[j].nome, x.partidos[i].deputados[j].genero, x.partidos[i].deputados[j].circulo);
+                            printf("\n");
+        printf("%s :\n",x.partidos[i].sigla);
+        for(j=0;j<(x.partidos[i].qtd);j++){
+            printf("%d - %s - %c - %s\n", x.partidos[i].deputados[j].codigo, x.partidos[i].deputados[j].nome, x.partidos[i].deputados[j].genero, x.partidos[i].deputados[j].circulo);
         }
     }
 }
@@ -61,7 +62,7 @@ void mostrardep (tParlamento x, int np,int nd){
 /*MAIN*/
 int main()
 {
-    int np=3,nd=12;
+    int np=3;
 
     /* Vetor inicial: Registo do País, os partidos do seu parlamento assim como os deputados que o compõem */
    tParlamento pt={"Portugal",{{"PS","Partido Socialista",4,{{1321,"Alexandre Tiedtke Quintanilha",'M',"Porto"},
@@ -70,7 +71,7 @@ int main()
    {{2045, "Pedro Manuel Mamede Passos Coelho",'M',"Lisboa"},{2778,"Luís Filipe Montenegro Cardoso de Morais Esteves",'M',"Aveiro"},{2356,"Maria Manuela Pereira Tender",'F',"Vila Real"},{2932,"Sara Martins Marques dos Santos Madruga da Costa",'F',"Madeira"},{2467,"Isaura Leonor Marques de Figueiredo Silva Pedro",'F',"Viseu"}}},{"BE","Bloco de Esquerda",3,{{3567,"Joana Rodrigues Mortagua",'F',"Setubal"},{3876,"Joao Manuel Duarte Vasconcelos",'M',"Faro"},{3004,"Catarina Soares Martins",'F',"Porto"}}}}};
    // lerparlamentos(pt, 3);
     mostrarparl(pt,np);
-    mostrardep(pt,np,nd);
+    mostrardep(pt,np);
     return 0;
 }
 
