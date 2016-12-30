e19b6959a70a9ae0fe065cfc36a2dae470b08ff5
albertosantos123 <aslms.tic@gmail.com>
2016-12-30 00:20:49 +0000

    Update inserirpartido.C

--- inserirpartido.C
+++ inserirpartido.C
@@ -3,33 +3,36 @@
 #define MAX 50
 
 /* Estrutura Deputado */
-typedef struct D{
-int codigo;
-char nome [MAX];
-char genero;
-char circulo[MAX];
-}tDeputado;
+typedef struct D
+{
+    int codigo;
+    char nome [MAX];
+    char genero;
+    char circulo[MAX];
+} tDeputado;
 
 /* Estrutura Deputados */
 typedef tDeputado tDeputados[MAX];
 
 /* Estrutura Partido */
-typedef struct P{
-char sigla[MAX];
-char nome[MAX];
-int qtd;
-tDeputados deputados;
-}tPartido;
+typedef struct P
+{
+    char sigla[MAX];
+    char nome[MAX];
+    int qtd;
+    tDeputados deputados;
+} tPartido;
 
 /* Estrutura Partidos */
 typedef tPartido tPartidos[MAX];
 
 /* Estrutura Parlamento */
-typedef struct Pa{
-char pais[MAX];
-tPartidos partidos;
-}tParlamento;
-
+typedef struct Pa
+{
+    char pais[MAX];
+    tPartidos partidos;
+} tParlamento;
+/*
 void mostrarpartid(tParlamento x,int np){
     printf("\nMostrar Partidos:");
     int i, j;
@@ -40,14 +43,41 @@ void mostrarpartid(tParlamento x,int np){
     }
     }
 }
-
+*/
+/*
 int inserirpart(tParlamento *x, int np){
     printf("Qual a sigla do novo partido?\n");
     gets((*x).partidos[np].sigla);
     printf("Qual o nome do partido?\n");
     gets((*x).partidos[np].nome);
+    printf("Qual a quantidade?\n");
+    scanf("%d",&(*x).partidos[np].qtd);
     np=np+1;
     return np;
+}
+*/
+void inserirdep(tParlamento *x, int np)
+{
+    char str[15];
+    int ret,i,cont=0;
+    printf("Qual a sigla do partido?\n");
+    gets(str);
+    for (i=0;i<np;i++){
+        ret=strcmp((*x).partidos[i].sigla,str);
+        if(ret==0){
+            printf("Qual o codigo do deputado?\n");
+            scanf("%d",&((*x).partidos[cont].deputados[((*x).partidos[cont].qtd)+1].codigo));
+            printf("Qual o nome do deputado??\n");
+            gets((*x).partidos[cont].deputados[((*x).partidos[cont].qtd)+1].nome);
+            printf("Qual o genero do deputado??\n");
+            scanf("%c",&((*x).partidos[cont].deputados[((*x).partidos[cont].qtd)+1].genero));
+            printf("Qual o circulo eleitoral do deputado??\n");
+            gets((*x).partidos[cont].deputados[((*x).partidos[cont].qtd)+1].circulo);
+        }
+        else
+            cont++;
+    }
+
 }
 
 
@@ -57,11 +87,17 @@ int main()
     int np=3;
 
     /* Vetor inicial: Registo do País, os partidos do seu parlamento assim como os deputados que o compõem */
-   tParlamento pt={"Portugal",{{"PS","Partido Socialista",4,{{1321,"Alexandre Tiedtke Quintanilha",'M',"Porto"},
-   {1456,"Antonio Paulo Jacinto Eusebio",'M',"Faro"},{1789,"Ana Catarina Veiga Santos Mendonca Mendes",'F',"Setubal"},
-   {1309,"Wanda Olavo Correa D'azevedo Guimaraes",'F',"Lisboa"}}},{"PSD","Partido Social Democrata",5,
-   {{2045, "Pedro Manuel Mamede Passos Coelho",'M',"Lisboa"},{2778,"Luis Filipe Montenegro Cardoso de Morais Esteves",'M',"Aveiro"},{2356,"Maria Manuela Pereira Tender",'F',"Vila Real"},{2932,"Sara Martins Marques dos Santos Madruga da Costa",'F',"Madeira"},{2467,"Isaura Leonor Marques de Figueiredo Silva Pedro",'F',"Viseu"}}},{"BE","Bloco de Esquerda",3,{{3567,"Joana Rodrigues Mortagua",'F',"Setubal"},{3876,"Joao Manuel Duarte Vasconcelos",'M',"Faro"},{3004,"Catarina Soares Martins",'F',"Porto"}}}}};
-   np=inserirpart(&pt, np);
-   mostrarpartid(pt,np);
-   return 0;
+    tParlamento pt= {"Portugal",{{"PS","Partido Socialista",4,{{1321,"Alexandre Tiedtke Quintanilha",'M',"Porto"},
+                    {1456,"Antonio Paulo Jacinto Eusebio",'M',"Faro"},{1789,"Ana Catarina Veiga Santos Mendonca Mendes",'F',"Setubal"},
+                    {1309,"Wanda Olavo Correa D'azevedo Guimaraes",'F',"Lisboa"}
+                }
+            },{"PSD","Partido Social Democrata",5,
+                {{2045, "Pedro Manuel Mamede Passos Coelho",'M',"Lisboa"},{2778,"Luis Filipe Montenegro Cardoso de Morais Esteves",'M',"Aveiro"},{2356,"Maria Manuela Pereira Tender",'F',"Vila Real"},{2932,"Sara Martins Marques dos Santos Madruga da Costa",'F',"Madeira"},{2467,"Isaura Leonor Marques de Figueiredo Silva Pedro",'F',"Viseu"}}
+            },{"BE","Bloco de Esquerda",3,{{3567,"Joana Rodrigues Mortagua",'F',"Setubal"},{3876,"Joao Manuel Duarte Vasconcelos",'M',"Faro"},{3004,"Catarina Soares Martins",'F',"Porto"}}}
+        }
+    };
+    //np=inserirpart(&pt, np);
+    inserirdep(&pt,np);
+    //mostrarpartid(pt,np);
+    return 0;
 }
